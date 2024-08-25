94a0367484a4a8329050e120c575261980a93ae1
Peter <pst_tan@yahoo.com>
2024-08-25 18:40:27 -0400

    2222nd commit

--- src/Length.java
+++ src/Length.java
@@ -3,6 +3,7 @@ public class Length {
 
 	public static void main(String[] args) {
 		  // remarks
+		  // rearks 2
 		  String name[]= {"Ali", "Boss","John","Paul"};
 		  
 		  for(int i=0; i<name.length; i++) {
